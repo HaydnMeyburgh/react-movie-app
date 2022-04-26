303c9ee8c8fe635829e41a07cb9c91eeffccad12
Haydn Meyburgh <haydnmey@gmail.com>
2022-04-26 14:51:00 +0200

    added footer information

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "./footer.scss";
+
 
 export const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="footer">
+      <div>Movie App</div>
+      <div>2022, Movie Inc. and all its affiliates</div>
     </div>
   )
 }
