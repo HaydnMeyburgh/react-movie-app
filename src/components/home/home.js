e97cf8538d6b8b33228a0619d9a4fcd08b826c28
Haydn Meyburgh <haydnmey@gmail.com>
2022-04-26 15:16:52 +0200

    added a fetch request to the OMDb api

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { MovieListing } from "../movieListing/movieListing";
+import movieApi from "../../common/apis/movieApi";
+import { apiKey } from "../../common/apis/movieApiKey";
 
 export const Home = () => {
+  //API fetch call
+  useEffect(() => {
+    const movieText = "Harry"
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${apiKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log("Err :", err);
+        });
+    console.log(response);
+
+    }
+    fetchMovies();
+  }, []); 
+
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </>
+    
   )
 }
