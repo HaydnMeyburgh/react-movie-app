f1ab0fb124d51e3f6a7955c133c9ef898b88a167
Haydn Meyburgh <haydnmey@gmail.com>
2022-04-27 12:02:13 +0200

    used props to grab data from API and add it to the movie card to display the movie information

--- src/components/movieCard/movieCard.js
+++ src/components/movieCard/movieCard.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "./movieCard.scss";
 
-export const MovieCard = () => {
+export const MovieCard = ({data}) => {
   return (
-    <div>
-      Movie Card
+    <div className="card-item">
+      <div className="card-inner">
+        <div className="card-top">
+          <img src={data.Poster} alt={data.Title} />
+        </div>
+        <div className="card-bottom">
+          <div className="card-info">
+            <h4>{data.Title}</h4>
+            <p>{data.Year}</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
