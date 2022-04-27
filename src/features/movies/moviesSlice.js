d7b49eb219845157a8114bc5dbda69510142e633
Haydn Meyburgh <haydnmey@gmail.com>
2022-04-27 12:52:14 +0200

    created async fetch using CreateAsyncThunk and added in a series object to store series as well

--- src/features/movies/moviesSlice.js
+++ src/features/movies/moviesSlice.js
@@ -1,7 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import movieApi from "../../common/apis/movieApi";
+import { apiKey } from "../../common/apis/movieApiKey";
+
+export const fetchAsyncMovies = createAsyncThunk('movies/fetchAsyncMovies', async () => {
+  const movieText = "Harry";
+  const response = await movieApi
+    .get(`?apiKey=${apiKey}&s=${movieText}&type=movie`)
+    return response.data;
+})
+export const fetchAsyncShows = createAsyncThunk('movies/fetchAsyncShows', async () => {
+  const seriesText = "Friends";
+  const response = await movieApi
+    .get(`?apiKey=${apiKey}&s=${seriesText}&type=series`)
+    return response.data;
+})
 
 const initialState = {
-  movies: {}
+  movies: {},
+  shows: {}
 }
 
 export const moviesSlice = createSlice({
@@ -10,11 +26,28 @@ export const moviesSlice = createSlice({
   reducers: {
     addMovies: (state, action) => {
       state.movies = action.payload;
-    }
+    },
+  },
+  extraReducers: {
+    [fetchAsyncMovies.pending]: () => {
+      console.log("Pending");
+    },
+    [fetchAsyncMovies.fulfilled]: (state, action) => {
+      console.log("fetched successfully!");
+      return {...state, movies: action.payload}
+    },
+    [fetchAsyncMovies.rejected]: () => {
+      console.log("rejected");
+    },
+    [fetchAsyncShows.fulfilled]: (state, action) => {
+      console.log("fetched successfully!");
+      return {...state, shows: action.payload}
+    },
   }
 });
 
-export const { addMovies } = moviesSlice.actions;
+export const { addMovies, addShows } = moviesSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
+export const getAllShows = (state) => state.movies.shows;
 export default moviesSlice.reducer;
 
