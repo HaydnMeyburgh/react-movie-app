9e545d20cdbdd85426da8744316cbd3ce8487966
Haydn Meyburgh <haydnmey@gmail.com>
2022-04-27 12:53:12 +0200

    added dispatch for async fetch series

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,31 +1,18 @@
 import React, { useEffect } from "react";
 import { MovieListing } from "../movieListing/movieListing";
-import movieApi from "../../common/apis/movieApi";
-import { apiKey } from "../../common/apis/movieApiKey";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../../features/movies/moviesSlice";
+import { fetchAsyncMovies, fetchAsyncShows } from "../../features/movies/moviesSlice";
 
 export const Home = () => {
-  const movieText = "Harry";
   const dispatch = useDispatch();
-  //API fetch call
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apiKey=${apiKey}&s=${movieText}&type=movie`)
-        .catch((err) => {
-          console.log("Err :", err);
-        });
-      dispatch(addMovies(response.data));
-    }
-    fetchMovies();
+    dispatch(fetchAsyncMovies());
+    dispatch(fetchAsyncShows());
   }, [dispatch]); 
-
   return (
     <div>
       <div className="banner-img"></div>
       <MovieListing />
-    </div>
-    
+    </div>   
   )
 }
