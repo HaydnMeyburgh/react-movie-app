16f72198bc2effe3598cfb5545c65ca49d4fa85f
Haydn Meyburgh <haydnmey@gmail.com>
2022-04-27 11:33:13 +0200

    used useEffect hook to dispatch addMovies action with data collected from API

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,28 +2,30 @@ import React, { useEffect } from "react";
 import { MovieListing } from "../movieListing/movieListing";
 import movieApi from "../../common/apis/movieApi";
 import { apiKey } from "../../common/apis/movieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../features/movies/moviesSlice";
 
 export const Home = () => {
+  const movieText = "Harry";
+  const dispatch = useDispatch();
   //API fetch call
   useEffect(() => {
-    const movieText = "Harry"
     const fetchMovies = async () => {
       const response = await movieApi
         .get(`?apiKey=${apiKey}&s=${movieText}&type=movie`)
         .catch((err) => {
           console.log("Err :", err);
         });
-    console.log(response);
-
+      dispatch(addMovies(response.data));
     }
     fetchMovies();
-  }, []); 
+  }, [dispatch]); 
 
   return (
-    <>
+    <div>
       <div className="banner-img"></div>
       <MovieListing />
-    </>
+    </div>
     
   )
 }
