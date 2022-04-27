7b1e2fc3d783f1c8711d80dce8092c1ab1f5a9be
Haydn Meyburgh <haydnmey@gmail.com>
2022-04-27 11:51:17 +0200

    rendered movies by selecting them from the store

--- src/components/movieListing/movieListing.js
+++ src/components/movieListing/movieListing.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getAllMovies } from "../../features/movies/moviesSlice";
+import { MovieCard } from "../movieCard/movieCard";
+import "./movieListing.scss";
 
 export const MovieListing = () => {
-  return (
-    <div>
-      Movie Listing
+  const movies = useSelector(getAllMovies);
+  let renderMovies = "";
+  renderMovies = movies.Response === "True" ? (
+    movies.Search.map((movie, index) => (
+      <MovieCard key={index} data={movie} />
+    ))
+    ) : (<div className="movies-error"><h3>{movies.error}</h3></div>);
+    return (
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">
+          {renderMovies}
+        </div>
+      </div>
     </div>
   )
 }
+
